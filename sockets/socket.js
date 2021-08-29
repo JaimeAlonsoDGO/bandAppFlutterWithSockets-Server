6ee352020907e94cea2a1ac1c958d284654cf3f8
Jaime Alonso Reyes Esparza <jaimealonsomail@gmail.com>
2021-08-29 15:36:14 -0500

    Done

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,20 @@
 const { io } = require("../index");
+const Band = require("../models/band");
+const Bands = require("../models/bands");
+
+const bands = new Bands();
+
+bands.addBand(new Band("Queen"));
+bands.addBand(new Band("The Killers"));
+bands.addBand(new Band("The National"));
+bands.addBand(new Band("The Black Keys"));
 
 // Mensajes Sockets
 io.on("connection", (client) => {
   console.log("Cliente Conectado");
+
+  // Envia al cliente las bandas al conectarse
+  client.emit("active-bands", bands.getBands());
 
   client.on("disconnect", () => {
     console.log("Cliente desconectado");
@@ -14,5 +26,31 @@ io.on("connection", (client) => {
 
     // io hace referencia a todo el servidor
     io.emit("mensaje", { admin: "Nuevo Mensaje" });
+  });
+
+  client.on("emitir-mensaje", (payload) => {
+    // console.log(payload);
+    // io.emit("mensaje", payload); //Para todos los clientes conectados
+    client.broadcast.emit("emitir-mensaje", payload); //Para todos, excepto quien lo emitió
+  });
+
+  // VOTE BAND
+  client.on("vote-band", (payload) => {
+    bands.voteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
+  });
+
+  // ADD BAND
+  client.on("add-band", (payload) => {
+    console.log(payload);
+    const newBand = new Band(payload.name);
+    bands.addBand(newBand);
+    io.emit("active-bands", bands.getBands());
+  });
+
+  // DELETE BAND
+  client.on("delete-band", (payload) => {
+    bands.deleteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
   });
 });
